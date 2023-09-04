cb239a58bf07031f903000ebbff77f625124fd52
Ftoupet <2132122@cmontmorency.qc.ca>
2023-09-04 16:47:16 -0400

    Affichage MDP

--- app/src/main/java/com/example/laboratoire1/MainActivity.java
+++ app/src/main/java/com/example/laboratoire1/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.laboratoire1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etMotDePasse;
+    private CheckBox cbAfficherMotDePasse;
+    private TextView tv_message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        etMotDePasse = findViewById(R.id.et_mot_de_passe);
+        tv_message = findViewById(R.id.tv_message);
+        //tv_message.setVisibility(View.TEXT); ici
+        cbAfficherMotDePasse = findViewById(R.id.cb_afficher_mot_de_passe);
+
+        cbAfficherMotDePasse.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                etMotDePasse.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            } else {
+                etMotDePasse.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            }
+        });
     }
+
 }
